cb63acc8951506287c2838fb14ab11b54ffab32f
senkaPro <isenatpro@gmail.com>
2019-06-04 02:53:28 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import psycopg2
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -79,9 +79,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME' : os.path.join(BASE_DIR,'oglasnikmk'),        
+        'HEROKU':{
+            'NAME': 'dm2ubueg46cur',
+        },
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
