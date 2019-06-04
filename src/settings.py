f1ad8773f4d4b8a5fc67276a924655c8593358fd
senkaPro <isenatpro@gmail.com>
2019-06-04 03:27:19 +0200

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -137,4 +137,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
-STATICFILES_STORAGE = 'whitenoise.django.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
