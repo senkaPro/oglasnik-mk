63037a9ed2dd001c00c277b59dec9c333fb4da77
senkaPro <isenatpro@gmail.com>
2020-01-15 03:23:07 +0100

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-6$%8252%=69ru9!-hoh#ekw(rp(1vqkr&&t%k(1f$x36x#($d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
